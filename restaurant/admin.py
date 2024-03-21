6bc20b173cbb6d1a181bbf395814ab0868704730
Roy <roy.mattar@outlook.com>
2024-03-22 01:35:16 +0200

    Setting up mysql

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+# In admin.py
+
+from .models import Menu, Booking
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'price', 'inventory']
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'guests', 'booking_date']
