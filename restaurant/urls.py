404596358d922be8be5ef6db1f0f536b8e79148e
Roy <roy.mattar@outlook.com>
2024-03-22 16:16:30 +0200

    Views, serializers created

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('tables/', views.BookingViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('tables/<int:pk>/', views.BookingViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
 ]
